92d1f611bae1450c2fa58e5b45829bf3e1595897
Raman Gupta <rocketraman@gmail.com>
2015-07-27 13:00:02 -0400

    Enable Redis storage backend for Swarm

--- src/core/swarmserver.js
+++ src/core/swarmserver.js
@@ -1,11 +1,22 @@
 import SwarmBase from 'swarm/lib/NodeServer'
 import swarmFactory from './swarmfactory'
+import redis from 'redis'
+import RedisStorage from '../vendor/swarm/RedisStorage'
 
 let Swarm = swarmFactory(SwarmBase)
 
-let fileStorage = new Swarm.FileStorage('.swarm')
+//let storage = new Swarm.FileStorage('.swarm')
+let storage = new RedisStorage({
+  redis: redis,
+  redisConnectParams: {
+    port: 6379,
+    host: '127.0.0.1',
+    options: {}
+  }
+})
+storage.open()
 
-Swarm.host = new Swarm.Host('swarm~nodejs', 0, fileStorage)
+Swarm.host = new Swarm.Host('swarm~nodejs', 0, storage)
 Swarm.env.localhost = Swarm.host
 
 export default Swarm
