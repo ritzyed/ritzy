0a90c153360c51816c94497dd2064cc3aecb730a
Raman Gupta <rocketraman@gmail.com>
2015-07-27 13:14:11 -0400

    Fix non-zero width in canvas-based measurement of newline chars

--- src/core/TextFontMetrics.js
+++ src/core/TextFontMetrics.js
@@ -66,6 +66,10 @@ function clearCanvas() {
 }
 
 function calcTextAdvanceCanvas(text, fontSize, font) {
+  // need to override newline handling, measureText doesn't handle it correctly (returns a non-zero width)
+  if(text === '\n') {
+    return 0
+  }
   let context = canvasContext()
   context.font = fontSpec(fontSize, font)
   return context.measureText(text).width
