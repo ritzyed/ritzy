d61310364a3493779f99f6587864398f6de13813
Raman Gupta <rocketraman@gmail.com>
2015-08-24 12:33:54 -0400

    Rendering optimization broke formatting
    The rendering optimization broke formatting. The line chars were being
    checked for equality from one render to the next, but not the line chunks
    (which contain the formatting attributes). Add chunks to the deep equality
    comparison for lines.
    
    Fixes #9.

--- src/core/EditorCommon.js
+++ src/core/EditorCommon.js
@@ -136,8 +136,8 @@ export function linesEq(line1, line2) {
   if(_.isArray(line1.chars) && _.isArray(line2.chars) && line1.chars.length !== line2.chars.length) return false
   if(!_.isEqual(line1.start, line2.start)) return false
   if(!_.isEqual(line1.end, line2.end)) return false
-  if(!_.isEqual(line1.chars, line2.chars)) return false
-  return true
+  if(!_.isEqual(line1.chunks, line2.chunks)) return false
+  return _.isEqual(line1.chars, line2.chars)
 }
 
 /**
